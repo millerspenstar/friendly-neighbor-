c689de05bd7df1767590705d44d9072663a00d31
Spencer Miller <millerspenstar@gmail.com>
2018-06-12 19:50:20 -0600

    assigned value with event

--- src/AcceptJob.js
+++ src/AcceptJob.js
@@ -12,16 +12,7 @@ export default class AcceptJob extends Component {
         super(props)
         this.state = { job:this.props.location.state}
     }
-    handleOnChange =(e) => {
-        this.setState((prev) => {
-            return {
-                job: {
-                    ...prev.job,
-                    name: e.target.value
-                }
-            }
-        })
-    }
+  
 
     
 
@@ -30,7 +21,8 @@ export default class AcceptJob extends Component {
         return(
         <section>
         <div><label>Name:</label><input type="text" onChange={(e)=>{
-            this.setState((prev,) => {
+            e.persist()
+            this.setState((prev) => {
                 return {
                     job: {
                         ...prev.job,
@@ -40,7 +32,8 @@ export default class AcceptJob extends Component {
             })
         }}/> </div>
         <div><label>Email:</label><input type="text" onChange={(e)=>{
-            this.setState((prev,) =>{
+            e.persist()
+            this.setState((prev) =>{
                 return {
                     email: {
                         ...prev.email,
@@ -50,6 +43,7 @@ export default class AcceptJob extends Component {
             })
         }}/></div>
         <div><label>Phone Number:</label><input type="text" onChange={(e)=>{
+            e.persist()
             this.setState((prev) =>{
                 return {
                     phoneNumber: {
